8c4d431e8b0611b76569ca2ebabca48502a481b1
occnah <ossian.stange@outlook.com>
2025-01-21 17:17:45 +0100

    Add and adjust projects

--- scripts/language.js
+++ scripts/language.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var dropdown = document.querySelector('.dropdown');
     var dropdownContent = document.querySelector('.dropdown-content');
     var dropdownLinks = dropdownContent.querySelectorAll('a');
     var visible = false;
 
-    dropdown.addEventListener('click', function(event) {
+    dropdown.addEventListener('click', function (event) {
         //event.stopPropagation(); // Prevent clicks inside dropdown from closing dropdown
         dropdownContent.style.display = visible ? "none" : "block";
         visible = !visible;
     });
 
     // Use event delegation to handle clicks on dropdown links
-    dropdownContent.addEventListener('click', function(event) {
+    dropdownContent.addEventListener('click', function (event) {
         if (event.target.tagName === 'A') {
             var index = Array.from(dropdownLinks).indexOf(event.target);
             setLanguage(index);
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Close dropdown when clicking outside of it
-    document.body.addEventListener('click', function(event) {
+    document.body.addEventListener('click', function (event) {
         if (visible && !dropdown.contains(event.target)) {
             dropdownContent.style.display = "none";
             visible = false;
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function setLanguage(index) {
-    var biotext =  document.getElementById("bio-text");
+    var biotext = document.getElementById("bio-text");
     var biolist = document.getElementById("bio-list");
     var biolistItems = biolist.querySelectorAll('li');
     var maintitle = document.getElementById("main-title");
@@ -47,12 +47,12 @@ function setLanguage(index) {
         case 1:
             maintitle.innerText = "Ossian Stange";
             biolistItems[0].innerText = "Web Designer & App Developer 💻";
-            biolistItems[1].innerText = "Computer Science student at KTH 🎓";
+            biolistItems[1].innerText = "MSc Student in Computer Science 🎓";
             biolistItems[2].innerText = "Stockholm/Jönköping 📍";
             break;
         case 2:
             maintitle.innerText = "Ossian Stange";
-            biolistItems[0].innerText = "Web- und Appentwickler 💻";
+            biolistItems[0].innerText = "Web- und Appentwickler";
             biolistItems[1].innerText = "Informatikstudent bei KTH 🎓";
             biolistItems[2].innerText = "Stockholm/Jönköping 📍";
             break;
@@ -62,14 +62,12 @@ function setLanguage(index) {
             biolistItems[1].innerText = "스웨덴왕립공과대학교 🎓";
             biolistItems[2].innerText = "스톡홀름 📍";
             break;
-        case 4:
-            maintitle.innerText = "オシアン スタンゲ";
-            biolistItems[0].innerText = "情報工学を専攻しています 💻";
-            biolistItems[1].innerText = "王立工科大学 🎓";
-            biolistItems[2].innerText = "ストックホルム 📍";
-
-            break;
         // Add more cases as needed
+        case 4:
+            maintitle.innerText = " オシアン スタンゲ";
+            biolistItems[0].innerText = " 情報工学を専攻しています 💻 ";
+            biolistItems[1].innerText = "王立工科大学 🎓";
+            biolistItems[2].innerText = " ストックホルム 📍";
         default:
             maintitle.innerText = "Ossian Stange";
             biotext.innerHTML = "";
